907c83873a7e2350de2ff3377b10b4dd98181533
Andy Zhong <Andy.Zhong@libertymutual.com%>
2018-10-22 13:37:40 -0400

    1.6 Added an App component

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Create a new component. This component should produce some HTML.
+
+const App = function() {
+  return <div>Hi!</div>
+}
+
+// Take this compnent's generated HTML and place it onto the page.
