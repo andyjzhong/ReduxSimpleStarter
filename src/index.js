20e078ab6f18ea07578d5ab649c1902d89570dc0
Andy Zhong <Andy.Zhong@LibertyMutual.com>
2018-10-29 09:11:06 -0400

    1.15 Add youtube search dependency

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom';
 const API_KEY = process.env.YOUTUBE_API_KEY;
 
 // Create a new component. This component should produce some HTML.
-
 const App = () => {
   console.log(API_KEY);
   return <div>Hi!</div>
