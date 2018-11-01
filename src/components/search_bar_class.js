f7dddffae5f41f8b9a605ac34ef08ecaf458561f
Andy Zhong <Andy.Zhong@LibertyMutual.com>
2018-11-01 07:50:17 -0400

    1.20 Return input value using setState

--- src/components/search_bar_class.js
+++ src/components/search_bar_class.js
@@ -5,11 +5,20 @@ class SearchBar extends Component {
     constructor(props) {
         super(props)
 
+        // Initializes state for SearchBar
         this.state = { searchTerm: '' }
     }
 
     render() {
-        return <input onChange={(event) => console.log(event.target.value)} />;
+        // Returns an input that contains an event handler to set the state of Search Bar to the input value
+        // Whenever the input is changed, display the input content by referencing it
+        return(
+            <div>
+                <input onChange={(event) => this.setState({ searchTerm: event.target.value })} />
+                <br />
+                Value of input: {this.state.searchTerm}
+            </div>
+        );
     }
 }
 
