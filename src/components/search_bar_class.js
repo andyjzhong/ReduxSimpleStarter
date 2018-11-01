683baa51f3d34256e7f2835bda221b2a43e21c56
Andy Zhong <Andy.Zhong@LibertyMutual.com>
2018-11-01 08:02:15 -0400

    1.21 Change input field into a controlled form field component

--- src/components/search_bar_class.js
+++ src/components/search_bar_class.js
@@ -6,15 +6,20 @@ class SearchBar extends Component {
         super(props)
 
         // Initializes state for SearchBar
-        this.state = { searchTerm: '' }
+        this.state = { searchTerm: 'Placeholder Value' }
     }
 
     render() {
         // Returns an input that contains an event handler to set the state of Search Bar to the input value
         // Whenever the input is changed, display the input content by referencing it
+        // When we tell the input that the value is provided by this.state.term, this makes it a controlled component
+        // Its value only changes when the state changes
+        // When the user types something, it doesn't change the value, it just triggers an event
         return(
             <div>
-                <input onChange={(event) => this.setState({ searchTerm: event.target.value })} />
+                <input
+                    value={this.state.searchTerm}
+                    onChange={(event) => this.setState({ searchTerm: event.target.value })} />
                 <br />
                 Value of input: {this.state.searchTerm}
             </div>
